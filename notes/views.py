2fe933fd0da82f1d6452c15eaf0b176cae28c728
Kangas Jyri V <jyri_kangas@outlook.com>
2022-12-31 22:16:12 +0200

    final comments

--- notes/views.py
+++ notes/views.py
@@ -5,6 +5,8 @@ from notes.models import Note
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
+
 def index(request):
     users = User.objects.all() 
     notes = Note.objects.all()
@@ -13,6 +15,7 @@ def index(request):
 
 def loginView(request):
     if request.method == "POST":
+        ## if tries > 3, return error
         username =request.POST.get("username")
         password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
@@ -26,10 +29,12 @@ def register(request):
     if request.method == "POST":
         username =request.POST.get("username")
         password = request.POST.get("password")
+        ##call a function to check password strength, if not strong enough, return error
         User.objects.create_user(username=username, password=password)
         return redirect('/loginView')
     return render(request, 'register.html', {"error": "register failed"})
 
+##login_required
 def add(request):
     if not request.user.is_authenticated:
         return render(request, 'login.html', {'error': "add fail"})
@@ -45,13 +50,19 @@ def logoutView(request):
     logout(request)
     return redirect('/')
 
+##login_required
 def accountView(request, username):
+    ##   if not request.user.is_authenticated:
+    ##   return render(request, 'login.html', {'error': "add fail"})
     pageowner = User.objects.get(username=username)
     notes = Note.objects.filter(user=pageowner)
     user=request.user
     return render(request, 'account.html', {"notes": notes, "user": user, "pageowner": pageowner})
 
+##login_required
 def deleteView(request, id):
+    ##   if not request.user.is_authenticated:
+    ##   return render(request, 'login.html', {'error': "add fail"})
     note = Note.objects.get(id=id)
     username=note.user.username
     note.delete()
